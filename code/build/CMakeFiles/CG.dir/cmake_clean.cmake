aadc1732dc72dcad1de082de543de6cd4f8b2115
Breno <marraob@gmail.com>
2023-05-29 17:56:55 +0100

    novo makelist

---
+++ code/build/CMakeFiles/CG.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CG"
+  "CG.pdb"
+  "CMakeFiles/CG.dir/main.cpp.o"
+  "CMakeFiles/CG.dir/main.cpp.o.d"
+  "CMakeFiles/CG.dir/tinyxml2.cpp.o"
+  "CMakeFiles/CG.dir/tinyxml2.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
